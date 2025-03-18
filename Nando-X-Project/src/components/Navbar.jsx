c10ed949949137a46371c76d22259f76665c797c
Nando <kommandernando@outlook.com>
2025-03-18 14:55:53 -0400

    Added Navbar and rudamentary homepage

---
+++ Nando-X-Project/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navbar.css';
+
+function CollapsibleNavbar() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Nando-X</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Forecast</Nav.Link>
+            <Nav.Link href="#pricing">Joke</Nav.Link>
+            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">GitHub Repo</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Nando's Repo</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">X's Repo</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Docs</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CollapsibleNavbar;
---
+++ Nando-X-Project/src/components/home-page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      {/* Centered H1 */}
+      <div>
+        <h1>Hello, Welcome To API-Hub!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
